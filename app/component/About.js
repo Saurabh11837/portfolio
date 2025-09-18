9650874257f82a94882a29fd343b41bdd2f319c8
Saurabh11837 <patelsaurabhroa@gmail.com>
2025-09-18 19:17:26 +0530

    About page scrolling set margintop-10 to 9

--- app/component/About.js
+++ app/component/About.js
@@ -17,7 +17,7 @@ const About = () => {
   }, []);
 
   return (
-    <section className="w-full h-full scroll-mt-10" id="About">
+    <section className="w-full h-full scroll-mt-9" id="About">
       {/* Heading */}
       <div className="flex flex-col justify-center items-center py-10">
         <p
